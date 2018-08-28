9445643f0899296a5f4359d7d6e9f6616caf801e
Abdul Wahab <abwahab708@gmail.com>
2018-08-28 08:08:20 +0500

    Primitive Types in JS
    JS is a light-weight, cross-platform and OO programming language. JS adds interactivity and dynamic effects to web pages.
    Primitive types in JS are Number, String, Boolean, Undefined and Null.
    Explored basic rules and conventions for naming JS variables.
    JS has dynamic typing, data types are assigned to variables automatically.

---
+++ script.js
@@ -0,0 +1,30 @@
+console.log('Hello World!!!');
+
+// Variables and Data Types
+var firstName = 'Abdul';
+var lastName = 'Wahab';
+var age = 25;
+var fullAge = false;
+var job;
+
+console.log(firstName); // String - Abdul
+console.log(lastName); // String - Wahab
+console.log(age); // Number - 25
+console.log(fullAge); // Boolean - false
+console.log(job); // undefined
+
+// Rules and Convention for naming variables
+/*
+  variable name can't start with the digit.
+  variable name can start with the letter, $ or _ only.
+  use camelcase when multiple words in variable name.
+  to name a variable you can't use JS reserved keywords like true, function, delete
+*/
+
+// Invalid declarations
+/*
+  var 3years = 3;
+  var delete = 'delete';
+*/
+
+// JS has dynamic typing, means types are assigned to variable automatically. 
